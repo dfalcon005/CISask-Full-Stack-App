d3c6413e6815e2d1d8e2e6aacb95ae54e8cbd6f2
dfalcon005 <dfalcon005@gmail.com>
2020-04-22 16:32:50 -0500

    Created login and signup pages layout

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,7 +1,30 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <div class="row justify-content-center align-items-center" style="height:100vh">
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body text-center login-header">
+                        <h1 class="navbar-brand login-logo">Univeristy of Houston cis.ask</h1>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body text-center">
+                        <form action="" autocomplete="off">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="email" placeholder="Email...">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password...">
+                            </div>
+                            <button type="button" id="sendlogin" class="btn btn-outline-dark">login</button>
+                        </form>
+                        <router-link to="Signup">Sign-up Now</router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+        
 </template>
 
 <script>
@@ -11,5 +34,14 @@ export default {
 </script>
 
 <style scoped>
+form{
+    padding-bottom: 1vh;
+}
+.login-header{
+    background-color: #dc3545;
+}
+.login-logo{
+    color: white;
+}
 
 </style>
--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -1,6 +1,31 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <div class="row justify-content-center align-items-center" style="height:100vh">
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body text-center login-header">
+                        <h1 class="navbar-brand login-logo">Univeristy of Houston cis.ask</h1>
+                        <h2 class="navbar-brand login-logo">Signup!</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body text-center">
+                        <form action="" autocomplete="off">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="email" placeholder="Choose a Username...">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="email" placeholder="UH Email...">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Choose a Password...">
+                            </div>
+                            <button type="button" class="btn btn-outline-dark">done</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +36,10 @@ export default {
 </script>
 
 <style scoped>
-
+.login-header{
+    background-color: #dc3545;
+}
+.login-logo{
+    color: white;
+}
 </style>
